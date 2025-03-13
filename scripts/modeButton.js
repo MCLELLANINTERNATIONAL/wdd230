a0c47c87cd71374ce91ce1bdf0de26cbf813bc84
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-03-13 22:46:59 +0000

    update

--- scripts/modeButton.js
+++ scripts/modeButton.js
@@ -3,12 +3,12 @@ const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
+		main.style.background = "rgba(255, 255, 0, 1";
 		main.style.color = "#fff";
 		modeButton.textContent = "🔆";
 	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
+		main.style.background = "#FFD1DC";
+		main.style.color = "#E75480";
 		modeButton.textContent = "🕶️";
 	}
 });
