d05e0343ace15ec63f5626bacf5890a5d0974948
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-03-31 10:36:07 +0100

    update

--- scripts/radioStar.js
+++ scripts/radioStar.js
@@ -1,5 +1,5 @@
 const rangevalue = document.getElementById("rangevalue");
-const range = document.getElementById("ratingvalue");
+const range = document.getElementById("range");
 
 // RANGE event listener
 range.addEventListener('change', displayRatingValue);
