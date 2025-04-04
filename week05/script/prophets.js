d91604c980c6973406c43cc173205020c319c210
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-04-04 23:43:55 +0100

    update

--- week05/script/prophets.js
+++ week05/script/prophets.js
@@ -18,26 +18,51 @@ getProphetData();
 // Step 5: Display the Prophets using DOM manipulation
 const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
-    // Create section card
+    // Create the main card
     let card = document.createElement('section');
-    let fullName = document.createElement('h2');
-    let portrait = document.createElement('img');
 
-    // Fill in content
+    // Create and fill the name heading
+    let fullName = document.createElement('h2');
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
 
-    // Set attributes for image
+    // Create image
+    let portrait = document.createElement('img');
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
-    // Append elements to the card
+    // Create and fill other data
+    let birthdate = document.createElement('p');
+    birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
+
+    let birthplace = document.createElement('p');
+    birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
+
+    let numOfChildren = document.createElement('p');
+    numOfChildren.textContent = `Number of Children: ${prophet.numofchildren}`;
+
+    let length = document.createElement('p');
+    length.textContent = `Length of Service: ${prophet.length} years`;
+
+    let order = document.createElement('p');
+    order.textContent = `Order of Presidency: ${prophet.order}`;
+
+    let death = document.createElement('p');
+    death.textContent = `Died: ${prophet.death}`;
+
+    // Append all elements to the card
     card.appendChild(fullName);
+    card.appendChild(birthdate);
+    card.appendChild(birthplace);
+    card.appendChild(numOfChildren);
+    card.appendChild(length);
+    card.appendChild(order);
+    card.appendChild(death);
     card.appendChild(portrait);
 
-    // Append the card to the cards container
+    // Append card to the cards container
     cards.appendChild(card);
   });
 };
