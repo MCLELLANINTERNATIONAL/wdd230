175bb8155df8f42a8473eefa521c14b84a92d75b
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-04-07 14:41:34 +0100

    update

--- week05/script/prophets.js
+++ week05/script/prophets.js
@@ -11,10 +11,14 @@ const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
     let card = document.createElement('section');
     let fullName = document.createElement('h2');
+    let birthdate = document.createElement('p');
+    let birthplace = document.createElement('p'); 
     let portrait = document.createElement('img');
 
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-
+    birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
+    birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
+    
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
@@ -22,6 +26,8 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('height', '440');
 
     card.appendChild(fullName);
+    card.appendChild(birthdate);
+    card.appendChild(birthplace);
     card.appendChild(portrait);
 
     cards.appendChild(card);
