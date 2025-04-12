3a61b6728ec1793384360fe22dbe1f401e3e0ca7
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-04-12 20:31:50 +0100

    update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -53,8 +53,8 @@ async function getWeather() {
         <div class="forecast-day">
           <p class="forecast-date"><strong>${FormattedDate}</strong></p>
           <img src="${iconUrl}" alt="${forecastDescription}">
-          <h4>${forecastDescription}</h4>
           <h4>${day.main.temp}°C</h4>
+          <h4>${forecastDescription}</h4>
         </div>
       `;
     });
