24da43be99bbccbe2cab1f3ed2f96a3815a7b191
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-04-13 22:57:33 +0100

    update

--- chamber/scripts/wayfinder.js
+++ chamber/scripts/wayfinder.js
@@ -1,31 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     const breadcrumbContainer = document.getElementById('breadcrumb');
     if (!breadcrumbContainer) return;
- 
-    const path = window.location.pathname.replace("/wdd230/chamber", "");
-    const pathSegments = path.split('/').filter(seg => seg && seg !== "index.html");
   
-    if (pathSegments.length === 0) {
-      breadcrumbContainer.style.display = "none";
-      return;
-    }
+    const path = window.location.pathname.replace("/wdd230/", "");
+    const segments = path.split('/').filter(seg => seg && seg !== 'index.html');
   
-    let breadcrumbHTML = `<a href="/wdd230/chamber/index.html">Home</a>`;
-    let pathAccumulator = "/wdd230/chamber";
+    let breadcrumbHTML = `
+      <a href="/wdd230/">Chamber</a> &gt; 
+      <a href="/wdd230/chamber/index.html">Home</a>
+    `;
   
-    pathSegments.forEach((segment, index) => {
-      pathAccumulator += `/${segment}`;
-      const formatted = segment
+    if (segments.length > 1 || (segments.length === 1 && segments[0] !== 'chamber')) {
+      const lastSegment = segments[segments.length - 1];
+      const formatted = lastSegment
         .replace(/[-_]/g, ' ')
         .replace(/\.\w+$/, '')
         .replace(/\b\w/g, l => l.toUpperCase());
   
-      if (index === pathSegments.length - 1) {
-        breadcrumbHTML += ` &gt; <span>${formatted}</span>`;
+      let link;
+      if (lastSegment.endsWith('.html')) {
+        link = `/wdd230/chamber/${lastSegment}`;
       } else {
-        breadcrumbHTML += ` &gt; <a href="${pathAccumulator}/index.html">${formatted}</a>`;
+        link = `/wdd230/chamber/${lastSegment}/index.html`;
       }
-    });
   
-    breadcrumbContainer.innerHTML = breadcrumbHTML;
+      breadcrumbHTML += ` &gt; <a href="${link}">${formatted}</a>`;
+    }
+  
+    if (segments.length === 0 || (segments.length === 1 && segments[0] === 'chamber')) {
+      breadcrumbContainer.style.display = 'none'; 
+    } else {
+      breadcrumbContainer.innerHTML = breadcrumbHTML;
+    }
   });
